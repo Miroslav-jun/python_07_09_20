a2160875d92850332ff715509380e158f4418172
Dev_mirik <miroslav_gr@mail.ru>
2020-09-14 19:19:07 +0300

    create task2

--- homeworks/lesson2/task2.py
+++ homeworks/lesson2/task2.py
@@ -3,25 +3,25 @@
 При нечетном количестве элементов последний сохранить на своем месте.
 Для заполнения списка элементов необходимо использовать функцию input().'''
 
-
 var_list = input('Введите значение:\n')
 i = 0
 if var_list.isdigit():
     var_list = list(var_list)
-    max_list = int(var_list.index(max(var_list)))
+    len_list = len(var_list)
+
     for items in var_list:
         if i == 0:
             items = var_list[1]
-        elif i % 2 == 1 and i != max_list:
-            items = var_list[i - 1]
-        elif i % 2 != 1 and i != max_list:
+        elif i % 2 == 0 and len_list % 2 != 1:
             items = var_list[i + 1]
+        elif i % 2 == 1 and len_list % 2 != 1:
+            items = var_list[i - 1]
         else:
-            items = var_list[max_list]
+            items = items
         i += 1
         print(items)
 else:
-    print('Не верно введены данные')
+    print('Неверно введены данные')
 
 
 
